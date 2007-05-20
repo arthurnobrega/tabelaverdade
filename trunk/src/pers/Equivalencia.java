ea33d0f4e4ad5e05507266923bc6090ee7cb04a1
arthurthiago <arthurthiago@c220f815-dd30-0410-9975-d76d301b4276>
2007-05-20 02:47:21 +0000

    Adicionando a minha parte do projeto inicial.

---
+++ trunk/src/pers/Equivalencia.java
@@ -0,0 +1,26 @@
+/*
+ * Equivalencia.java
+ *
+ */
+
+package pers;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Arthur Thiago Barbosa Nobrega
+ */
+public class Equivalencia {
+    
+    /** Creates a new instance of Equivalencia */
+    public Equivalencia() {
+    }
+    
+    /* Esta função retorna se não ocorreu nenhum erro no uso do objeto. */
+    public int verificarSucesso() {
+        return sucesso;
+    }
+    
+    private int sucesso = 0;
+}
