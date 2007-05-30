65efe68d67dde506122c340ae1504f9ec425828d
felippepipipe <felippepipipe@c220f815-dd30-0410-9975-d76d301b4276>
2007-05-30 16:54:04 +0000

    Criação do módulo de lógica

--- TabelaVerdade/src/log/Logica.java
+++ TabelaVerdade/src/log/Logica.java
@@ -14,8 +14,83 @@ import tipos.Formula;
  */
 public class Logica {
     
-    /* Esta função irá testar se a fórmula é bem formada e segue os padrões exigidos.*/
-    protected boolean testarFormula(Formula formula) {
+    private int parentese = 0;
+    private int operadores = 0;
+    
+    /* Transforma a formula recebida em um vetor de caracteres*/
+    private char[] StringToChar(String formula){
+        
+        char[] cVetor = new char[formula.length()];
+        
+        for(int i=0; i <= formula.length() - 1; i++){
+            cVetor[i] = formula.charAt(i); 
+            if(cVetor[i] == '('){
+                parentese +=1;
+            }
+            else if(cVetor[i] == '^' || cVetor[i] == 'v' || cVetor[i] == '-' ||
+                    cVetor[i] == '~' || cVetor[i] == '='){
+                    operadores += 1;
+                }
+        }
+        return cVetor;
+    }
+    
+    
+    private boolean testarOperadoresJuntos(char[] formula){
+        
+        int tamanhoVetor = formula.length;
+        
+        for(int i=0;i<(tamanhoVetor-2);){
+            do{
+                i++; 
+            }while(formula[i] != '^' && formula[i] != 'v' && formula[i] !=
+                    '-' && formula[i] != '~' && formula[i] != '=');
+            
+            if(formula[i+1] == '^' || formula[i+1] == 'v' || formula[i+1] == '-'
+                    || formula[i+1] == '~' || formula[i+1] == '='){
+                return false;
+            }
+        }
+                
         return true;
     }
+    
+    
+    /* Esta funï¿½ï¿½o irï¿½ testar se a fï¿½rmula ï¿½ bem formada e segue os padrï¿½es exigidos.*/
+    protected boolean testarFormulaBemFormada(Formula formula) {
+        
+        String strFormula = formula.getFormula();
+        char[] cVetor = StringToChar(strFormula);
+        
+        
+        if(parentese != operadores || parentese == 0){
+            return false;
+        }        
+        
+        for(int i=0;i<strFormula.length()-1;i++){
+            if(cVetor[i] == '(' && cVetor[i+1] == ')'){
+                return false;
+            }
+        }    
+        
+        if(!(testarOperadoresJuntos(cVetor))){
+            System.out.println("faungo");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public static void main(String[] args){
+        Logica log = new Logica();
+        
+        Formula x = new Formula("(~(p^q))");
+        if(log.testarFormulaBemFormada(x))
+            System.out.println("true");              
+        else
+            System.out.println("false");
+                
+                
+    }
+    
 }
