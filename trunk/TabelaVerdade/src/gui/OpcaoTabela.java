a00c80561d1da84ffe5ea52c45d646014502a7eb
arthurthiago <arthurthiago@c220f815-dd30-0410-9975-d76d301b4276>
2007-05-31 11:43:29 +0000

    Alterei alguns acentos para UNICODE para que não tenhamos mais problemas com isso.

--- trunk/TabelaVerdade/src/gui/OpcaoTabela.java
+++ trunk/TabelaVerdade/src/gui/OpcaoTabela.java
@@ -29,24 +29,16 @@ public class OpcaoTabela extends javax.swing.JFrame {
      */
     // <editor-fold defaultstate="collapsed" desc=" Código Gerado ">//GEN-BEGIN:initComponents
     private void initComponents() {
-        jScrollPane1 = new javax.swing.JScrollPane();
-        txtAjuda = new javax.swing.JTextArea();
         txtFormula = new javax.swing.JTextField();
         btnVoltar = new javax.swing.JButton();
         btnVisualizar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Visualizando a Tabela Verdade de uma FBF");
-        txtAjuda.setBackground(new java.awt.Color(226, 226, 226));
-        txtAjuda.setColumns(20);
-        txtAjuda.setEditable(false);
-        txtAjuda.setFont(new java.awt.Font("Courier", 0, 12));
-        txtAjuda.setLineWrap(true);
-        txtAjuda.setRows(5);
-        txtAjuda.setText("Digite a fórmula que você deseja visualizar a tabela verdade. Para isso siga os seguintes padrões:\n\n" + Constantes.PADRAO_FORMULA);
-        txtAjuda.setWrapStyleWord(true);
-        jScrollPane1.setViewportView(txtAjuda);
+        setResizable(false);
 
         btnVoltar.setText("Voltar");
         btnVoltar.addActionListener(new java.awt.event.ActionListener() {
@@ -64,6 +56,15 @@ public class OpcaoTabela extends javax.swing.JFrame {
 
         jLabel1.setText("F\u00f3rmula:");
 
+        jTextArea1.setBackground(new java.awt.Color(226, 226, 226));
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Arial", 0, 13));
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setRows(5);
+        jTextArea1.setText("Digite a f\u00f3rmula que voc\u00ea deseja visualizar a tabela verdade. Para isso siga os seguintes padr\u00f5es:\n\n" + Constantes.PADRAO_FORMULA);
+        jTextArea1.setWrapStyleWord(true);
+        jScrollPane1.setViewportView(jTextArea1);
+
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -71,34 +72,34 @@ public class OpcaoTabela extends javax.swing.JFrame {
             .add(layout.createSequentialGroup()
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 694, Short.MAX_VALUE)
-                            .add(layout.createSequentialGroup()
-                                .add(btnVoltar)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 478, Short.MAX_VALUE)
-                                .add(btnVisualizar))))
-                    .add(layout.createSequentialGroup()
                         .add(10, 10, 10)
                         .add(jLabel1)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(txtFormula, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 648, Short.MAX_VALUE)))
+                        .add(txtFormula, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 697, Short.MAX_VALUE))
+                    .add(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .add(btnVoltar)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 527, Short.MAX_VALUE)
+                        .add(btnVisualizar))
+                    .add(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 743, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .addContainerGap()
-                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 217, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .add(21, 21, 21)
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 232, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(jLabel1)
                     .add(txtFormula, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .add(20, 20, 20)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 20, Short.MAX_VALUE)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(btnVoltar)
                     .add(btnVisualizar))
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -109,7 +110,7 @@ public class OpcaoTabela extends javax.swing.JFrame {
             LogicaTabela logTab = new LogicaTabela(formula);
             new Tabelas(this, true, logTab.getLinhas(), logTab.getColunas(), null).setVisible(true);
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "A fórmula não segue os padrões estipulados!","Fórmula incorreta!", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, Constantes.MENSAGEM_ERRO_FORMULA, Constantes.TITULO_ERRO_FORMULA, JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_btnVisualizarActionPerformed
 
@@ -123,7 +124,7 @@ public class OpcaoTabela extends javax.swing.JFrame {
     private javax.swing.JButton btnVoltar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea txtAjuda;
+    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField txtFormula;
     // Fim da declaração de variáveis//GEN-END:variables
     
