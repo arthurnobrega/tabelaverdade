06a8cff03af45e4f8a948cabe71f170c53ad0eae
felippepipipe <felippepipipe@c220f815-dd30-0410-9975-d76d301b4276>
2007-05-31 13:23:51 +0000

    Arrumei o Logica.java.

--- TabelaVerdade/src/log/Logica.java
+++ TabelaVerdade/src/log/Logica.java
@@ -15,7 +15,15 @@ import tipos.Formula;
  */
 public class Logica {
     
-    /* Esta função irá testar se a fórmula é bem formada e segue os padrões exigidos. */
+    /** 
+     * Esta função irá testar se a fórmula é bem formada e segue os padrões exigidos.
+     *ela utiliza funções para analizar o uso de operadores e proposições e casos
+     *de erro possíveis na construção da fórmula pelo usuário.
+     *
+     * @param formula Objeto que possui a fórmula como uma String.
+     *
+     * @return Retorna verdadeiro ou falso, caso seja uma FBF ou não, respectivamente. 
+     */
     protected boolean testarFormulaBemFormada(Formula formula) {
         
         String strFormula = formula.getFormula();
@@ -40,7 +48,14 @@ public class Logica {
         
     }  
     
-    /* Transforma a formula recebida em um vetor de caracteres. */
+    /**
+     * Transforma a formula recebida em um vetor de caracteres através de um loop. 
+     *
+     * @param formula String onde está armazenada a fórmula.
+     *
+     * @return Retorna um vetor de String's, utilizado para manipular cada
+     * símbolo da fórmula
+     */
     private String[] StringToChar(String formula){
         
         String[] cVetor = new String[formula.length()];
@@ -59,7 +74,16 @@ public class Logica {
     }
     
     
-    /* Função que testa a existência de dois operadores lógicos juntos. */
+    /** 
+     * Função que testa a existência de dois operadores lógicos juntos. 
+     *Se existe um parentese '(' iniciando a formula e um parentese ')' encerrando
+     *a fórmula.
+     *Ela analisa a existência de um um conector e um parentese ')' juntos.
+     *
+     * @param formula Vetor de Strings.
+     *
+     * @return Retorna um booleano verificando a utilização dos conectores.
+     */
     private boolean testarConectivosJuntos(String[] formula) {
         
         int tamanhoVetor = formula.length;
@@ -83,10 +107,27 @@ public class Logica {
         if (!(formula[tamanhoVetor-1].equals(")"))) {
             return false;
         }
+        
+        for(int i=0; i< tamanhoVetor - 1; i++){
+            if ((formula[i].equals(Constantes.CONJUNCAO)) || (formula[i].equals(Constantes.DISJUNCAO))
+            || (formula[i].equals(Constantes.NEGACAO)) || (formula[i].equals(Constantes.IMPLICACAO))
+            || (formula[i].equals(Constantes.DUPLA_IMPLICACAO))) {
+                 if(formula[i+1].equals(")")){
+                     return false;
+                 }
+             }
+        }
+        
         return true;
     }
     
-    /* Função que testa o número de proposições juntas. */
+    /**
+     * Função que testa se existi proposições juntas.
+     *
+     * @param formula Vetor de strings.
+     *
+     * @return Retorna um booleano depois de verificar a posição das proposições.
+     */
     private boolean testarProposicoesJuntas(String[] formula) {
         
         int tamanhoVetor = formula.length;
@@ -110,4 +151,5 @@ public class Logica {
     
     private int parenteses = 0;
     private int operadores = 0;
+    
 }
